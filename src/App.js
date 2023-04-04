23f929964f78404e97e8ad6c83c0e610c768795e
vuckovicstrahinja <strahinja.vuckovic@htecgroup.com>
2023-04-04 09:39:54 +0200

    Finish styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import logo from "./logo.svg";
 import "./App.css";
 import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
+import React, { useState } from "react";
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (username, userage) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: username, age: userage, id: Math.random().toString() },
+      ];
+    });
+  };
+
   return (
     <div>
-      <AddUser></AddUser>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
     </div>
   );
 }
